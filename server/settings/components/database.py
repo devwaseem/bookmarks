0c618ad0c07d63d475555f32226104b39d3d13ca
Waseem Akram <waseem07799@gmail.com>
2024-01-06 19:04:17 +0530

    feat: use sqlite for db

--- server/settings/components/database.py
+++ server/settings/components/database.py
@@ -1,20 +1,25 @@
+from pathlib import Path
 from env import Env
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    # "default": {
+    #     "ENGINE": "django.db.backends.postgresql",
+    #     "NAME": Env("POSTGRES_DB"),
+    #     "USER": Env("POSTGRES_USER"),
+    #     "PASSWORD": Env("POSTGRES_PASSWORD"),
+    #     "HOST": Env("DJANGO_DATABASE_HOST"),
+    #     "PORT": Env("DJANGO_DATABASE_PORT", int, 5432),
+    #     "CONN_MAX_AGE": Env("CONN_MAX_AGE", int, 60),
+    #     "OPTIONS": {
+    #         "connect_timeout": 10,
+    #         "options": "-c statement_timeout=15000ms",
+    #     },
+    # },
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": Env("POSTGRES_DB"),
-        "USER": Env("POSTGRES_USER"),
-        "PASSWORD": Env("POSTGRES_PASSWORD"),
-        "HOST": Env("DJANGO_DATABASE_HOST"),
-        "PORT": Env("DJANGO_DATABASE_PORT", int, 5432),
-        "CONN_MAX_AGE": Env("CONN_MAX_AGE", int, 60),
-        "OPTIONS": {
-            "connect_timeout": 10,
-            "options": "-c statement_timeout=15000ms",
-        },
-    },
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": Env("SQLITE_DB_PATH", str, Path().cwd() / "data.db"),
+    }
 }
