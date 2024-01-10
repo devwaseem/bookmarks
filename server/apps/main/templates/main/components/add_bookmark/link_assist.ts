4abf358361a525e60582602bfb5b121511e82557
Waseem Akram <waseem07799@gmail.com>
2024-01-11 00:14:21 +0530

    feat: add loading states

--- server/apps/main/templates/main/components/add_bookmark/link_assist.ts
+++ server/apps/main/templates/main/components/add_bookmark/link_assist.ts
@@ -1,28 +1,30 @@
 import Alpine from 'alpinejs';
 
 Alpine.data('AddBookmarkLinkAssist', () => ({
-    title: null,
-    link: null,
-    init() {
-        //@ts-ignore
-        this.link = this.$refs.link.value;
-        //@ts-ignore
-        this.title = this.$refs.title.value;
-        console.log(this.link);
+  title: null,
+  link: null,
+  init() {
+    //@ts-ignore
+    this.link = this.$refs.link.value;
+    //@ts-ignore
+    this.title = this.$refs.title.value;
 
-        this.$watch('link', (value, oldValue) => {
-            console.log(value, oldValue);
-            if (value != oldValue && value != '') {
-                this.runAssist();
-            }
-        });
+    this.$watch('link', (value, oldValue) => {
+      console.log(value, oldValue);
+      if (value != oldValue && value != '') {
+        this.runAssist();
+      }
+    });
 
-        if (this.link != '') {
-            this.runAssist();
-        }
-    },
+    setTimeout(() => {
+      if (this.link != '') {
+        this.runAssist();
+      }
 
-    runAssist() {
-        this.$dispatch('add_bookmark:assist');
-    },
+    }, 100)
+  },
+
+  runAssist() {
+    this.$dispatch('add_bookmark:assist');
+  },
 }));
